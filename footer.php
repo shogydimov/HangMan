7af3b7f3edb577538fdbd193bd1304b970038416
Georgi Dimov <shogydimov@gmail.com>
2017-08-21 14:46:58 +0300

    change footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
             <hr>
         </div>
         <footer class="footer">
-            <p class="footer">Георги Димов 2017 &reg;</p>
+            <p class="footer">Георги Димов 08.2017 &reg;</p>
             <p class="footer">Последвай ме във Facebook: <a class="footer" href="https://www.facebook.com/SH0GY" target="_blank"><i class="fa fa-facebook"></i></a></p>
         </footer>
     </div>
